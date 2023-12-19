6f713ff9d21361b58799303362b4ae48b7dcb236
Dharmesh Panchal <dharmesh351991@gmail.com>
2023-12-19 15:40:57 +0530

    Promises for multiple API calls

--- indexPro-async-wait.js
+++ indexPro-async-wait.js
@@ -16,18 +16,46 @@ const writeFilePro = (file, data) => {
         });
     })
 }
+//Option 1 - using single API call to get image
+// const getDogname = async () => {
+//     try {
+//         const dog_name = await readFilePro('dog_name.txt');
+//         console.log('Dog name', dog_name);
+//         const response = await superagent.get(`https://dog.ceo/api/breed/${dog_name}/images/random`);
+//         let imageURL = response.body.message;
+//         console.log('Image URL', imageURL);
+//         await writeFilePro('dog_image.txt', imageURL);
+//         console.log('Random image has been saved to text file....');
+//     }
+//     catch (error) {
+//         console.log('Error', error);
+//     }
+//     return "Ready : ✨"
+// };
+//Option 1 - using multiple API call to get images
 const getDogname = async () => {
     try {
         const dog_name = await readFilePro('dog_name.txt');
         console.log('Dog name', dog_name);
-        const response = await superagent.get(`https://dog.ceo/api/breed/${dog_name}/images/random`);
-        let imageURL = response.body.message;
-        console.log('Image URL', imageURL);
-        await writeFilePro('dog_image.txt', imageURL);
+        const res_1 = superagent.get(`https://dog.ceo/api/breed/${dog_name}/images/random`);
+        const res_2 = superagent.get(`https://dog.ceo/api/breed/${dog_name}/images/random`);
+        const res_3 = superagent.get(`https://dog.ceo/api/breed/${dog_name}/images/random`);
+        const response = await Promise.all([res_1, res_2, res_3]);
+        const images = response.map(imageData => imageData.body.message );
+        console.log('Images URL', images);
+        await writeFilePro('dog_image.txt', images.join('\n'));
         console.log('Random image has been saved to text file....');
     }
     catch (error) {
         console.log('Error', error);
     }
+    return "Ready : ✨"
 };
-getDogname();
+(async () =>{
+    const x = await getDogname();
+    console.log('We will get image... 🛴');
+    console.log(x);
+    console.log('We have recieved image');
+})()
+
+
